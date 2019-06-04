fef31aa7f2aa142f7842f4702842c8c9783a5dd6
Andrew Duong <andrewduong77@yahoo.com>
2019-06-04 00:57:29 -0500

    hmwk 01 directory created

---
+++ HMWK_01_and7697/hmwk_01.cs
@@ -0,0 +1,13 @@
+// Dalio, Brian A.
+// dalioba
+// 2019-06-03
+
+using System;
+
+public class hmwk_01
+{
+  static public void Main( string[] args )
+  {
+    // Put an output statement here that prints "Hello, world!"
+  }
+}
---
+++ HMWK_01_and7697/hmwk_01.java
@@ -0,0 +1,10 @@
+// Dalio, Brian A.
+// dalioba
+// 2019-06-03
+
+public class hmwk_01 {
+  public static void main( String[] args )
+  {
+    // Put an output statement here that prints "Hello, world!"
+  }
+}
