12bb44d986a3b5547e52edcb2dcea4ba1b2c02a6
Andrew <andrewduong77@yahoo.com>
2019-06-06 12:47:19 -0500

    completed hmwk_02, tested, and added more files

---
+++ hmwk_02_and7697/hmwk_02.c
@@ -0,0 +1,10 @@
+// Duong, Andrew
+// and7697
+// 2019-06-06
+
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+  printf("Hello, world!\n");
+}
---
+++ hmwk_02_and7697/hmwk_02.cc
@@ -0,0 +1,12 @@
+// Duong, Andrew
+// and7697
+// 2019-06-06
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char *argv[] )
+{
+  cout << "Hello, world!" << endl;
+}
