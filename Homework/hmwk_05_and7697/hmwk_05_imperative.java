85e65f8fb02034e5cfb1a7e91f419bf889b03520
Andrew <andrewduong77@yahoo.com>
2019-06-26 10:52:36 -0500

    downloaded hmwk 05 and lambdas handout

---
+++ Homework/hmwk_05_and7697/hmwk_05_imperative.java
@@ -0,0 +1,31 @@
+// Duong, Andrew
+// and7697
+// 2019-06-26
+
+public class hmwk_05_imperative {
+  //----------------------------------------------------------
+  // Sum of cubes
+  static Long sumOfCubes( Long n ) {
+    // Put something here
+  }
+
+  // Pseudo-Pell numbers
+  static Long pseudoPell( Long n ) {
+    // Put something here
+  }
+
+  // GCD
+  static Long GCD( Long m, Long n ) {
+    // Put something here
+  }
+
+  //----------------------------------------------------------
+  public static void main( String[] args )
+  {
+    // Put for loop here that calls sumOfCubes().
+
+    // Put for loop here that calls pseudoPell().
+
+    // Put nested for loops here that call GCD().
+  }
+}
---
+++ Homework/hmwk_05_and7697/hmwk_05_lambda.java
@@ -0,0 +1,25 @@
+// Duong, Andrew
+// and7697
+// 2019-06-26
+
+import java.util.function.UnaryOperator;
+import java.util.function.BinaryOperator;
+
+public class hmwk_05_lambda {
+  //----------------------------------------------------------
+  // sumOfCubes lambda goes here.
+
+  // pseudoPell lambda goes here.
+
+  // GCD lambda goes here.
+
+  //----------------------------------------------------------
+  public static void main( String[] args )
+  {
+    // Put for loop here that applies the sumOfCubes lambda.
+
+    // Put for loop here that applies the pseudoPell lambda.
+
+    // Put for loop here that applies the GCD lambda.
+  }
+}
