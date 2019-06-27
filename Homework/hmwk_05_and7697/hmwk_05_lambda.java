67b0eb98b28170a80fe58e3d35b4223d39d76207
Andrew <andrewduong77@yahoo.com>
2019-06-27 11:40:12 -0500

    finished hmwk 05 completely, tested and ran successfully, correct output verified with diff

--- Homework/hmwk_05_and7697/hmwk_05_lambda.java
+++ Homework/hmwk_05_and7697/hmwk_05_lambda.java
@@ -7,53 +7,24 @@ import java.util.function.BinaryOperator;
 
 public class hmwk_05_lambda {
   //----------------------------------------------------------
-  // sumOfCubes lambda goes here.
   static UnaryOperator<Integer>
     sumOfCubes = ( Integer n ) -> ( n == 0 ) ? 0 : n*n*n + hmwk_05_lambda.sumOfCubes.apply( n - 1 );
 
-  // pseudoPell lambda goes here.
   static UnaryOperator<Integer>
     pseudoPell = ( Integer n ) -> ( n == 0 ) ? 0 : ( n == 1 ) ? 1 : 3 * hmwk_05_lambda.pseudoPell.apply( n - 1 ) + 2 * hmwk_05_lambda.pseudoPell.apply( n - 2 );
 
-  // GCD lambda goes here.
   static BinaryOperator<Integer>
-    GCD = ( Integer m, Integer n ) -> ( m > n ) ? 
-    // GCD = ( Integer m, Integer n ) ->
-    // {
-    //   int remainder, numerator, denominator;
-
-    //   if( m > n )
-    //   {
-    //     numerator = m;
-    //     denominator = n;
-    //   }
-    //   else
-    //   {
-    //     numerator = n;
-    //     denominator = m;
-    //   }
-    //   remainder = numerator % denominator;
-    //   while( remainder != 0 )
-    //   {
-    //     numerator = denominator;
-    //     denominator = remainder;
-    //     remainder = numerator % denominator;
-    //   }
-    //   return denominator;
-    // };
+    GCD = ( Integer m, Integer n ) -> ( m > n ) ? ( m % n == 0 ? n : hmwk_05_lambda.GCD.apply( n, m % n ) ) : ( n % m == 0 ? m : hmwk_05_lambda.GCD.apply( m, n % m ) );
 
   //----------------------------------------------------------
   public static void main( String[] args )
   {
-    // Put for loop here that applies the sumOfCubes lambda.
     for ( int i = 0; i <= 15; i++ )
       System.out.println( "sumOfCubes(" + i + ") is " + sumOfCubes.apply( i ) );
 
-    // Put for loop here that applies the pseudoPell lambda.
     for ( int i = 0; i <= 15; i++ )
       System.out.println( "pseudoPell(" + i + ") is " + pseudoPell.apply( i ) );
 
-    // Put for loop here that applies the GCD lambda.
     for ( int i = 1; i <= 10; i++ )
       for ( int j = 1; j <= 10; j++ )
         System.out.println( "GCD(" + i + ", " + j + ") is " + GCD.apply( i, j ) );
